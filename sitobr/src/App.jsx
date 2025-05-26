a749a62910396503bbc9eb392286d42d7ce7e55d
Federico Di Presa <federicodipresa94@gmail.com>
2025-05-26 16:33:01 +0200

    Add footer

--- sitobr/src/App.jsx
+++ sitobr/src/App.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import Header from "./components/Header";
 import FilterBar from "./components/FilterBar";
 import EventList from "./components/EventList";
-
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <div>
       <Header />
       <FilterBar />
       <EventList />
+      <Footer />
     </div>
   );
 };
---
+++ sitobr/src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaInstagram, FaTiktok } from "react-icons/fa";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="social-icons">
+        <a
+          href="https://www.instagram.com/drinkbrit?igsh=MWxveTRjcmx2eHR0eA=="
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram">
+          <FaInstagram className="icon" />
+        </a>
+        <a
+          href="https://www.tiktok.com/@drinkbrit?_t=ZN-8wgCPAGdklq&_r=1"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="TikTok">
+          <FaTiktok className="icon" />
+        </a>
+      </div>
+      <p className="copyright">
+        &copy; 2023 - P.IVA 13110090969 - Tutti i diritti riservati
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
